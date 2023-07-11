f70d115603738b022e83d2a586626ce94cc4a3f6
namlukoh <lukoh.nam@bigpearl.io>
2023-07-11 12:07:45 +0900

    Applied Chrome CustomTab Client

--- app/src/main/java/com/goforer/base/customtab/OpenCustomTab.kt
+++ app/src/main/java/com/goforer/base/customtab/OpenCustomTab.kt
@@ -3,12 +3,15 @@ package com.goforer.base.customtab
 import android.content.ComponentName
 import android.content.Context
 import android.graphics.Color
+import android.os.Bundle
 import android.util.Patterns
 import androidx.browser.customtabs.CustomTabColorSchemeParams
+import androidx.browser.customtabs.CustomTabsCallback
 import androidx.browser.customtabs.CustomTabsClient
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.browser.customtabs.CustomTabsServiceConnection
 import com.goforer.base.utils.connect.ConnectionUtils.getUri
+import timber.log.Timber
 
 fun openCustomTab(context: Context, url: String) {
     val packageName = "com.android.chrome"
@@ -26,7 +29,25 @@ fun createCustomTabsServiceConnection(context: Context, packageName: String) {
         ) {
             client = customTabsClient
             client?.warmup(0L)
-            client?.newSession(null)
+            client?.newSession(object : CustomTabsCallback() {
+                override fun onNavigationEvent(navigationEvent: Int, extras: Bundle?) {
+                    super.onNavigationEvent(navigationEvent, extras)
+                    when (navigationEvent) {
+                        NAVIGATION_STARTED -> {
+                            Timber.d("NAVIGATION_STARTED")
+                        }
+                        NAVIGATION_FINISHED -> {
+                            Timber.d("NAVIGATION_FINISHED")
+                        }
+                        NAVIGATION_FAILED -> {
+                            Timber.d("NAVIGATION_FAILED")
+                        }
+                        NAVIGATION_ABORTED -> {
+                            Timber.d("NAVIGATION_ABORTED")
+                        }
+                    }
+                }
+            })
         }
 
         override fun onServiceDisconnected(name: ComponentName) {
@@ -46,7 +67,8 @@ fun loadContent(context: Context, url: String, color: Int = Color.TRANSPARENT) {
         val colorScheme = CustomTabsIntent.COLOR_SCHEME_LIGHT
         val customTabsIntent = CustomTabsIntent.Builder()
             .setColorSchemeParams(colorScheme, params)
-            .setShowTitle(false)
+            .setStartAnimations(context, android.R.anim.slide_out_right, android.R.anim.slide_in_left)
+            .setExitAnimations(context, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
             .setUrlBarHidingEnabled(true)
             .build()
 
